f9300d0d930eff3809ffb5857ce1f254ecada60f
Felix <fedecraft3312@gmail.com>
2024-10-14 09:29:21 -0300

    Fixed a problem with environment initialization

--- src/env/index.ts
+++ src/env/index.ts
@@ -12,35 +12,42 @@ export function initializeDotenv() {
 	if (isTesting()) {
 		dotenv.config({
 			path: ".env.testing",
+			override: true,
 		});
 	} else if (isDevelopment()) {
 		dotenv.config({
 			path: ".env.development",
+			override: true,
 		});
 	} else {
 		dotenv.config({
 			path: ".env.production",
+			override: true,
 		});
 	}
-	
+
 	// Then the local env
 	dotenv.config({
 		path: ".env.local",
+		override: true,
 	});
-	
+
 	// Specific locals
 	if (isTesting()) {
 		// Then the development env
 		dotenv.config({
 			path: ".env.testing.local",
+			override: true,
 		});
 	} else if (isDevelopment()) {
 		dotenv.config({
 			path: ".env.development.local",
+			override: true,
 		});
 	} else {
 		dotenv.config({
 			path: ".env.production.local",
+			override: true,
 		});
 	}
 }
@@ -49,60 +56,76 @@ export function initializeDotenv() {
  * Unbiased database name
  */
 export function defaultDatabaseName() {
-	return process.env.MYSQL_DATABASE_NAME ?? process.env.DATABASE_NAME ?? "perseverancia";
+	return (
+		process.env.MYSQL_DATABASE_NAME ??
+		process.env.DATABASE_NAME ??
+		"perseverancia"
+	);
 }
 
 /**
  * Get development database name
  */
 export function developmentDatabaseName() {
-	return process.env.MYSQL_DATABASE_NAME ?? process.env.DATABASE_NAME ?? "perseverancia-development";
+	return (
+		process.env.MYSQL_DATABASE_NAME ??
+		process.env.DATABASE_NAME ??
+		"perseverancia-development"
+	);
 }
 
 /**
  * Production database name
  */
 export function productionDatabaseName() {
-    return process.env.PRODUCTION_MYSQL_DATABASE_NAME ?? process.env.PRODUCTION_DATABASE_NAME ?? "perseverancia-production";
+	return (
+		process.env.PRODUCTION_MYSQL_DATABASE_NAME ??
+		process.env.PRODUCTION_DATABASE_NAME ??
+		"perseverancia-production"
+	);
 }
 
 /**
  * Testing database name
  */
 export function testingDatabaseName() {
-    return process.env.TESTING_MYSQL_DATABASE_NAME ?? process.env.TESTING_DATABASE_NAME ?? "perseverancia-testing";
+	return (
+		process.env.TESTING_MYSQL_DATABASE_NAME ??
+		process.env.TESTING_DATABASE_NAME ??
+		"perseverancia-testing"
+	);
 }
 
 /**
  * Check if it's testing mode
  */
 export function isTesting() {
-    return process.env.NODE_ENV === 'testing';
+	return process.env.NODE_ENV === "testing";
 }
 
 /**
  * Check if it's development
  */
 export function isDevelopment() {
-	return !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+	return !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 }
 
 /**
  * Use NODE_ENV to get the database name
- * 
+ *
  * Preferred for detection of database name
- * 
+ *
  * Note that to prevent wrongly synchronizing the database each one of the
  * methods requires a specific definition of the variable name in the environment
  */
 export default function databaseName() {
-    if (process.env.NODE_ENV === "production") {
-        return productionDatabaseName();
-    } else if (isDevelopment()) {
-        return developmentDatabaseName();
-    } else if (isTesting()) {
+	if (process.env.NODE_ENV === "production") {
+		return productionDatabaseName();
+	} else if (isDevelopment()) {
+		return developmentDatabaseName();
+	} else if (isTesting()) {
 		return testingDatabaseName();
 	} else {
-        return defaultDatabaseName();
-    }
+		return defaultDatabaseName();
+	}
 }
