9c7f801133a12a8d5cd23d8c59935d66c1826e4e
Felix <fedecraft3312@gmail.com>
2024-11-01 15:54:36 -0300

    Refresh tokens

---
+++ src/model/oauth/OAuthRefreshToken.ts
@@ -0,0 +1,86 @@
+import {
+	Model,
+	InferCreationAttributes,
+	InferAttributes,
+	Sequelize,
+	DataTypes,
+} from "sequelize";
+import User from "../user/User";
+import OAuth2Client from "./OAuth2Client";
+
+/**
+ * Address
+ */
+export default class OAuthRefreshToken extends Model<
+	InferAttributes<OAuthRefreshToken>,
+	InferCreationAttributes<
+		OAuthRefreshToken,
+		{ omit: "id" | "createdAt" | "updatedAt" }
+	>
+> {
+	declare id: number;
+
+	declare refreshToken: string;
+	declare refreshTokenExpiresAt: Date;
+	declare scope: string;
+
+	declare createdAt: Date;
+	declare updatedAt: Date;
+}
+
+/**
+ * Authorization token
+ */
+export function createOAuthRefreshToken(
+	conn: Sequelize,
+	oauth2Client: typeof OAuth2Client,
+	user: typeof User
+) {
+	const TABLE_NAME = "oauth-refresh-token";
+	const model = OAuthRefreshToken.init(
+		{
+			id: {
+				type: DataTypes.BIGINT,
+				primaryKey: true,
+				allowNull: false,
+				autoIncrement: true,
+			},
+			refreshToken: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			refreshTokenExpiresAt: {
+				type: DataTypes.DATE,
+				allowNull: false,
+			},
+			scope: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			createdAt: DataTypes.DATE,
+			updatedAt: DataTypes.DATE,
+		},
+		{
+			sequelize: conn,
+			tableName: TABLE_NAME,
+			modelName: TABLE_NAME,
+		}
+	);
+
+	// Should belong to user and client
+	model.belongsTo(user, {
+		foreignKey: "userId",
+		onUpdate: "CASCADE",
+		onDelete: "CASCADE",
+		targetKey: "id",
+	});
+
+	model.belongsTo(oauth2Client, {
+		foreignKey: "clientId",
+		onUpdate: "CASCADE",
+		onDelete: "CASCADE",
+		targetKey: "clientId",
+	});
+
+	return model;
+}
